a0d1d7c5da735973a5f066d69a1fa2ea77098e00
Samuel-5485 <samediriba54@gmail.com>
2025-10-11 13:17:15 +0300

    I have created class book and it's constructors

---
+++ Projects/book.py
@@ -0,0 +1,15 @@
+# Project: Mini Library Management System
+
+    # Features we can include:
+    # Add a book – store book info (title, author, year).
+    # Display all books – show the list of books.
+    # Search a book – search by title.
+    # Save & Load books – use a file so data persists.
+    # OOP structure – use classes like Book and Library.
+
+class book:
+    def __init__(self, title, author, year):
+        self.title = title
+        self.author = author
+        self.year = year
+
